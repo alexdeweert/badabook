be00eabe1e1e076e21bda5c8ab8eb0b704b9f005
Alex L. Deweert <alexander.deweert@gmail.com>
2025-02-01 13:04:52 -0700

    test commit

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Foo: typeof import('./src/components/Foo.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
   }
 }
